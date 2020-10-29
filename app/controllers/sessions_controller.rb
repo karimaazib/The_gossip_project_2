64f1ca0db23f03989b992658ef5aee71f312a14e
karimaazib <azib.karima@gmail.com>
2020-10-29 15:43:14 +0100

    Projet_Karima

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  def new  #correspond à la page de login
+  end
+
+  def create #effectuera l'authentification (traitement des informations saisies dans la page login et sauvegarde de l'info de l'utilisateur connecté dans session
+    # cherche s'il existe un utilisateur en base avec l’e-mail
+    user = User.find_by(email: params[:user_email])
+
+    # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe 
+    if user && user.authenticate(params[:user_password])
+      log_in(user)
+
+       # on va cuisiner le cookie pour l'utilisateur
+      remember(user)
+
+      redirect_to gossips_path(current_user), notice: "Vous êtes connecté" #redirige vers la page d'acceuil 
+    
+    else
+     flash.now.alert = "Invalid email or password"
+    render "new"
+    end
+  end
+
+  def destroy #correspond à la déconnection
+    #session[:user_id] = nil 
+    log_out(current_user)
+    redirect_to gossips_path
+  end
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+require 'bcrypt'
+
+class User < ApplicationRecord
+	belongs_to :city, optional: true
+  has_many :gossips
+  has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
+  has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
+  has_many :comments
+  has_many :likes, dependent: :destroy
+
+  has_secure_password
+
+ 
+  validates :pseudo, presence: true, uniqueness: true
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 3 }
+
+  include BCrypt
+
+  def password_authentification(mdp_confirmation, user)
+    
+  end
+
+  def remember(remember_token)
+    remember_digest = BCrypt::Password.create(remember_token)
+    self.update(remember_digest: remember_digest)
+  end
+
+end
